97b09e6b2ed05aceb424474a92d6d11253a29bce
SOHTA0318 <nicosohta0318@gmail.com>
2023-01-05 18:47:19 -0500

    initial commit

---
+++ user/schema/schema.js
@@ -0,0 +1,32 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const users = [
+  {
+    id: "23",
+    firstName: "Bill",
+    age: 20,
+  },
+  { id: "47", firstName: "Samantha", ag: 21 },
+];
+
+const UserType = new GraphQLObjectType({
+  name: "user",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {},
+    },
+  },
+});
