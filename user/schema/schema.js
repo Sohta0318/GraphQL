c4b39f857a62cfc847e578d52feb2bac91ecdafb
SOHTA0318 <nicosohta0318@gmail.com>
2023-01-09 13:40:01 -0500

    lesson13 garphql tool

--- user/schema/schema.js
+++ user/schema/schema.js
@@ -1,32 +1,41 @@
-const graphql = require("graphql");
-const _ = require("lodash");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
 
 const users = [
-  {
-    id: "23",
-    firstName: "Bill",
-    age: 20,
-  },
-  { id: "47", firstName: "Samantha", ag: 21 },
+      { id: '23', firstName: 'Bill', age: 20 },
+      { id: '47', firstName: 'Samantha', age: 21 }
 ];
 
 const UserType = new GraphQLObjectType({
-  name: "user",
+  name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt },
-  },
+    age: { type: GraphQLInt }
+  }
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQuery",
+  name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {},
-    },
-  },
+      args: { id:{ type: GraphQLString } },
+      // used for retrieve actual information from database, args parameter in resolve takes the value from args defined above
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
