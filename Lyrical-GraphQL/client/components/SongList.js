53859d6a394a5ad53ee1ad1c8327a0946bf712b4
SOHTA0318 <nicosohta0318@gmail.com>
2023-01-14 00:52:10 -0500

    deal with first render problem

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-const SongList = () => {
-  return <div>SongList</div>
+const SongList = (props) => {
+  if (props.data.loading) return <div>Loading...</div>
+  const renderSongs = () => {
+    return props.data.songs.map((song) => {
+      return <li key={song.id}>{song.title}</li>
+    })
+  }
+  return <ul className="collection">{renderSongs()}</ul>
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `
 
-export default SongList
+export default graphql(query)(SongList)
