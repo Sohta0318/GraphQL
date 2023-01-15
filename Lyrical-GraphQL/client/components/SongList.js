46f96298cc693e5e38eeea1a916bf951959a590a
SOHTA0318 <nicosohta0318@gmail.com>
2023-01-15 12:34:25 -0500

    delete mutation

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -6,9 +6,29 @@ import query from '../queries/fetchSong.js'
 
 const SongList = (props) => {
   if (props.data.loading) return <div>Loading...</div>
+  const onSongDelete = (id) => {
+    props.mutate({
+      variables: {
+        id,
+      },
+      fetchQueries: { query },
+    })
+  }
   const renderSongs = () => {
-    return props.data.songs.map((song) => {
-      return <li key={song.id}>{song.title}</li>
+    return props.data.songs.map(({ id, title }) => {
+      return (
+        <li key={id}>
+          {title}
+          <i
+            className="material-icons"
+            onClick={() => {
+              onSongDelete(id)
+            }}
+          >
+            delete
+          </i>
+        </li>
+      )
     })
   }
   return (
@@ -21,4 +41,12 @@ const SongList = (props) => {
   )
 }
 
-export default graphql(query)(SongList)
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`
+
+export default graphql(mutation)(graphql(query)(SongList))
