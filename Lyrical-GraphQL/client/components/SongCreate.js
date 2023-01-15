90ec17f0bbd2a6f53b07276e125eef4341d6caad
SOHTA0318 <nicosohta0318@gmail.com>
2023-01-14 21:14:53 -0500

    lesson48 handle mutation

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 class SongCreate extends Component {
   constructor(props) {
     super(props)
     this.state = { title: '' }
   }
+  onSubmit(event) {
+    event.preventDefault()
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    })
+  }
   render() {
     return (
       <div>
         <h3>Create a new song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             onChange={(e) => {
@@ -23,4 +34,12 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`
+
+export default graphql(mutation)(SongCreate)
