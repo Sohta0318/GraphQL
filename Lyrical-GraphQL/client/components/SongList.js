cec92cc6318b50d8b66923e389f71d1708c0e911
SOHTA0318 <nicosohta0318@gmail.com>
2023-01-15 11:58:21 -0500

    style & history

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 const SongList = (props) => {
   if (props.data.loading) return <div>Loading...</div>
@@ -9,7 +10,14 @@ const SongList = (props) => {
       return <li key={song.id}>{song.title}</li>
     })
   }
-  return <ul className="collection">{renderSongs()}</ul>
+  return (
+    <div>
+      <ul className="collection">{renderSongs()}</ul>
+      <Link to="/songs/new" className="btn-floating btn-large red right">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
+  )
 }
 
 const query = gql`
