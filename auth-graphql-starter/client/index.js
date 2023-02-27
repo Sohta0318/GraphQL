3ad981fe7b1dac85549890aa2a9fbb50a2eb1f28
SOHTA0318 <nicosohta0318@gmail.com>
2023-02-26 22:34:37 -0500

    lesson115 requireAuth & done

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -6,6 +6,8 @@ import { Router, hashHistory, Route } from 'react-router'
 import App from './components/App'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
+import Dashboard from './components/Dashboard'
+import requireAuth from './components/requireAuth'
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -24,8 +26,9 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path={'/'} component={App}>
-          <Route path={'/login'} component={LoginForm} />
-          <Route path={'/signup'} component={SignupForm} />
+          <Route path={'login'} component={LoginForm} />
+          <Route path={'signup'} component={SignupForm} />
+          <Route path={'dashboard'} component={requireAuth(Dashboard)} />
         </Route>
       </Router>
     </ApolloProvider>
